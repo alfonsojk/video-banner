28065419f7a084c43f468ce05c0d1608393c6b2f
alfonsojk <alfonso.juarezkelly@gmail.com>
2020-06-15 23:43:21 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector(".switch-btn");
+const video = document.querySelector(".video-container");
+
+
+
+// preloader
+const preloader = document.querySelector(".preloader");
+
+window.addEventListener("load", function () {
+  preloader.classList.add("hide-preloader");
+});
